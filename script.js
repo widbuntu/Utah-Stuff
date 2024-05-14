7bf9eb2636944aa3a32418847daedc612bf31dfd
Tyler <widbuntu@gmail.com>
2024-05-14 17:28:27 -0600

    Form addition

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ $(document).ready(function(){
         autoPlay:true
     });
 });
+
+document.getElementById('confirmEmail').addEventListener('input', function() {
+    const email = document.getElementById('email').value;
+    const confirmEmail = this.value;
+    const error = document.getElementById('emailError');
+
+    if (email !== confirmEmail) {
+        error.style.display = 'inline';
+    } else {
+        error.style.display = 'none';
+    }
+});
+
+function validateForm() {
+    const email = document.getElementById('email').value;
+    const confirmEmail = document.getElementById('confirmEmail').value;
+
+    if (email !== confirmEmail) {
+        alert('Email addresses do not match.');
+    } else {
+        alert('Form submitted successfully!');
+    }
+}
